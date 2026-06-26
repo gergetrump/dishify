81dd54eabdea4c11fcd2c40ab970212d0fb42557
Georg Tichy <georg.tichy@tum.de>
2026-06-26 23:12:43 +0200

    Set up MantineUI.

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,6 +1,8 @@
+import "@mantine/core/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
 
 import { App } from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
@@ -8,10 +10,12 @@ import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </MantineProvider>
   </React.StrictMode>,
 );
